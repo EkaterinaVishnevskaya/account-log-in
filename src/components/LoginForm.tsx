631df649cd4baa4d9ec0a5741bc6420d589bdd96
EkaterinaVishnevskaya <ekaterina2-97@yandex.ru>
2022-08-26 12:50:31 +0300

    fghj

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,8 +4,8 @@ import { Button, Checkbox, Form, Input } from 'antd';
 export default function LoginForm () { 
     return(
         <Form wrapperCol={{ span: 6 }}>
-            <Form.Item><Input className='form-item' placeholder="Логин или E-mail" prefix={<UserOutlined />} /></Form.Item>
-            <Form.Item><Input.Password className='form-item' placeholder="Введите пароль" prefix={<LockFilled />} /></Form.Item>
+            <Form.Item><Input className='form-item' placeholder="Логин или E-mail" prefix={<UserOutlined style={{color: '#1890ff',}} />} /></Form.Item>
+            <Form.Item><Input.Password className='form-item' placeholder="Введите пароль" prefix={<LockFilled style={{color: '#1890ff',}} />} /></Form.Item>
             <Form.Item>
                 <Checkbox>Запомнить меня</Checkbox></Form.Item><a className='forgot'>Забыли пароль?</a>
             <Form.Item><Button className='form-item button' type='primary' size='large'>Войти</Button></Form.Item>
