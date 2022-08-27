af403ade5e67db4d0c39b90f10eeefdaf279a22f
EkaterinaVishnevskaya <ekaterina2-97@yandex.ru>
2022-08-27 20:54:20 +0500

    dxfcghjkl

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -4,9 +4,9 @@ import { Button, Form, Input } from 'antd';
 export default function RegistrationForm () { 
     return(
         <Form wrapperCol={{ span: 6 }}>
-            <Form.Item><Input className='form-item' placeholder="Адрес электронной почты" prefix={<UserOutlined />} /></Form.Item>
-            <Form.Item><Input.Password className='form-item' placeholder="Введите пароль" prefix={<LockFilled />} /></Form.Item>
-            <Form.Item><Input.Password className='form-item' placeholder="Подтвердите пароль" prefix={<LockFilled />} /></Form.Item>
-            <Form.Item><Button className='form-item button' type='primary' size='large'>Зарегистироваться</Button></Form.Item>  
+            <Input className='form-item' placeholder="Адрес электронной почты" prefix={<UserOutlined />} />
+            <Input.Password className='form-item' placeholder="Введите пароль" prefix={<LockFilled />} />
+            <Input.Password className='form-item' placeholder="Подтвердите пароль" prefix={<LockFilled />} />
+            <Button className='form-item button' type='primary' size='large'>Зарегистироваться</Button>
         </Form>
 )}
