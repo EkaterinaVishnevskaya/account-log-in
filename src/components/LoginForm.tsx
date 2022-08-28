c8ee2d092eb6b45c6f738692216cce235daf8f94
EkaterinaVishnevskaya <ekaterina2-97@yandex.ru>
2022-08-28 22:00:09 +0500

    asdfffffffffffffffffffffffffffffff

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,13 +1,14 @@
 import { LockFilled, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
+import './LoginForm.css'
 
 export default function LoginForm () { 
     return(
         <Form wrapperCol={{ span: 6 }}>
-            <Input className='form-item' placeholder="Логин или E-mail" prefix={<UserOutlined style={{color: '#1890ff',}} />} />
-            <Input.Password className='form-item' placeholder="Введите пароль" prefix={<LockFilled style={{color: '#1890ff',}} />} />
-             <Checkbox>Запомнить меня</Checkbox><a className='forgot'>Забыли пароль?</a>
-            <Button className='form-item button' type='primary' size='large'>Войти</Button>
+            <Form.Item><Input className='form-item' placeholder="Логин или E-mail" prefix={<UserOutlined style={{color: '#1890ff',}} />} /></Form.Item>
+            <Form.Item><Input.Password className='form-item' placeholder="Введите пароль" prefix={<LockFilled style={{color: '#1890ff',}} />} /></Form.Item>
+            <Form.Item className='line'><Checkbox>Запомнить меня</Checkbox><a className='forgot'>Забыли пароль?</a></Form.Item>
+            <Form.Item><Button className='form-item button' type='primary' size='large'>Войти</Button></Form.Item>
             <p>Войти с помощью:</p>  
         </Form>
 )}
--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,12 +1,13 @@
 import { LockFilled, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
+import './RegistrationForm.css'
 
 export default function RegistrationForm () { 
     return(
-        <Form wrapperCol={{ span: 6 }}>
-            <Input className='form-item' placeholder="Адрес электронной почты" prefix={<UserOutlined />} />
-            <Input.Password className='form-item' placeholder="Введите пароль" prefix={<LockFilled />} />
-            <Input.Password className='form-item' placeholder="Подтвердите пароль" prefix={<LockFilled />} />
-            <Button className='form-item button' type='primary' size='large'>Зарегистироваться</Button>
+        <Form>
+            <Form.Item><Input className='form-item' placeholder="Адрес электронной почты" prefix={<UserOutlined />} /></Form.Item>
+            <Form.Item><Input.Password className='form-item' placeholder="Введите пароль" prefix={<LockFilled />} /></Form.Item>
+            <Form.Item><Input.Password className='form-item' placeholder="Подтвердите пароль" prefix={<LockFilled />} /></Form.Item>
+            <Form.Item><Button className='form-item button' type='primary' size='large'>Зарегистироваться</Button></Form.Item>
         </Form>
 )}
